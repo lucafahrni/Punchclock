74e39fb85f214c3318269b29b89747639f39955a
Luca Fahrni <lucafahrni@yahoo.com>
2020-11-26 05:10:38 -0800

    added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO Category(name)
+VALUES ('Faul'), ('Fleissig'), ('Mittelmässig'), ('InDenFerien');
+
+INSERT INTO ROLE(role_id)
+VALUES ('admin'), ('user');
+
+INSERT INTO USER(password, username)
+VALUES ('lololol', 'hanspeter'), ('soos', 'peter'), ('ichnicht', 'dochdoch');
+
+
+
+INSERT INTO ENTRY(check_in, check_out, applicationUser_id)
+VALUES ('2099-03-17 22:30:39', '2051-01-28 15:27:21', 2), ('1994-12-04 21:20:44', '1950-11-26 17:38:46', 3), ('1914-08-10 08:51:03', '2000-11-27 20:11:07', 1);
